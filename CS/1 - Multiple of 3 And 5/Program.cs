83434a10d28cd0c9e2f51d8d769bfc54732b0452
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-10-05 00:34:12 +0545

    Problem 1 Solved

---
+++ CS/1 - Multiple of 3 And 5/Program.cs
@@ -0,0 +1,19 @@
+namespace MultipleOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
+}
