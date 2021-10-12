91f76307f3e24ee99053e436c33f6a62faca9ba4
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-10-12 10:57:43 +0545

    Sum Square Difference code added

---
+++ CS/6 - Sum Square Difference/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SumSquareDifference
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            var num = Convert.ToInt32(Console.ReadLine());
+            double sumOfSquare = 0, squareOfSum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                sumOfSquare += Math.Pow(i, 2);
+                squareOfSum += i;
+            }
+            squareOfSum = Math.Pow(squareOfSum, 2);
+            Console.WriteLine($"Difference between the sum square is {squareOfSum - sumOfSquare}");
+            Console.ReadKey();
+        }
+    }
+}
